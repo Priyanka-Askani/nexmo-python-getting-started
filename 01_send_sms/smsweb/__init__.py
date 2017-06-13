6b37a386079c9f4ce512f1d8732a26a1f79b8910
Mark Smith <mark.smith@practicalpoetry.co.uk>
2017-06-13 13:41:37 +0100

    Working example.

---
+++ 01_send_sms/smsweb/__init__.py
@@ -0,0 +1,52 @@
+from flask import Flask, flash, redirect, render_template, request, url_for
+import nexmo
+
+from .util import env_var, extract_error
+
+
+# Load in configuration from environment variables:
+NEXMO_API_KEY = env_var('NEXMO_API_KEY')
+NEXMO_API_SECRET = env_var('NEXMO_API_SECRET')
+NEXMO_NUMBER = env_var('NEXMO_NUMBER')
+
+# Create a new Nexmo Client object:
+nexmo_client = nexmo.Client(
+    api_key=NEXMO_API_KEY, api_secret=NEXMO_API_SECRET
+)
+
+# Initialize Flask:
+app = Flask(__name__)
+
+# We need this for sessions to work - take note of the value!
+app.config['SECRET_KEY'] = 'DO NOT EVER USE IN PRODUCTION'
+
+
+@app.route('/')
+def index():
+    """ A view that renders the Send SMS form. """
+    return render_template('index.html')
+
+
+@app.route('/send_sms', methods=['POST'])
+def send_sms():
+    """ A POST endpoint that sends an SMS. """
+
+    # Extract the form values: 
+    to_number = request.form['to_number']
+    message = request.form['message']
+
+    # Send the SMS message:
+    result = nexmo_client.send_message({
+        'from': NEXMO_NUMBER,
+        'to': to_number,
+        'text': message,
+    })
+
+    err = extract_error(result)
+    if err is not None:
+        flash("There was a problem sending your message: " + err, 'error')
+    else:
+        flash("You just sent a message to " + to_number)
+
+    # Redirect the user back to the form:
+    return redirect(url_for('index'))
